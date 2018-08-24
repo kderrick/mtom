712ad5307b1aec4eeaa5297d0a0773850dd9aba9
Kyle <kderrick1988@gmail.com>
2018-08-24 14:17:24 -0700

    add FileWsImpl and implement upload method

---
+++ src/main/java/com/example/demo/FileWsImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.activation.DataHandler;
+
+public class FileWsImpl implements FileWs {
+
+	@Override
+	public void upload(DataHandler attachment) {
+		try {
+			InputStream inputStream = attachment.getInputStream();
+			OutputStream outputStream = new FileOutputStream(new File("/Users/kylederrick/Desktop/files/test.jpg"));
+			byte[] b = new byte[100000];
+			int bytesRead = 0;
+			
+			while((bytesRead=inputStream.read(b))!=1) {
+				outputStream.write(b, 0, bytesRead);
+			}
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public DataHandler download() {
+		return null;
+	}
+
+}
